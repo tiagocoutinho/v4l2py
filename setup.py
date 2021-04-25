3f5f71ab81a3175d049bdf897cc0a12c32d7525d
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-04-25 15:18:17 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md") as f:
 
 setup(
     name="v4l2py",
-    version="0.3.0",
+    version="0.4.0",
     author="Jose Tiago Macara Coutinho",
     author_email="coutinhotiago@gmail.com",
     license="GPLv3",
--- v4l2py/__init__.py
+++ v4l2py/__init__.py
@@ -1,3 +1,3 @@
 from .device import Device
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
