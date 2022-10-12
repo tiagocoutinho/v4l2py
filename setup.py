1e800febe540bc5ab82973b98b9e6476befdc4d6
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-10-12 15:21:52 +0200

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md") as f:
 
 setup(
     name="v4l2py",
-    version="0.6.1",
+    version="0.6.2",
     author="Jose Tiago Macara Coutinho",
     author_email="coutinhotiago@gmail.com",
     license="GPLv3",
--- v4l2py/__init__.py
+++ v4l2py/__init__.py
@@ -6,4 +6,4 @@
 
 from .device import Device
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
