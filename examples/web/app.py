d4e5d93af9fe18c6b96768363ce72df4672fbd80
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-02-15 18:38:27 +0100

    Update web example

--- examples/web/app.py
+++ examples/web/app.py
@@ -7,6 +7,8 @@
 # run from this directory with: 
 # gunicorn --bind=0.0.0.0:8000 --log-level=debug --worker-class=gevent app:app
 
+import functools
+import io
 import logging
 
 import flask
@@ -16,9 +18,12 @@ import gevent.monkey
 import gevent.queue
 import gevent.time
 
+from PIL import Image
+import pillow_avif
+
 gevent.monkey.patch_all()
 
-from v4l2py import Device, iter_video_capture_devices
+from v4l2py.device import Device, VideoCapture, Format, PixelFormat, iter_video_capture_devices
 
 app = flask.Flask("basic-web-cam")
 app.jinja_env.line_statement_prefix = "#"
@@ -30,12 +35,10 @@ logging.basicConfig(
 
 
 BOUNDARY = "frame"
-PREFIX = b"--frame\r\nContent-Type: image/jpeg\r\n\r\n"
-SUFFIX = b"\r\n"
-
 HEADER = (
     "--{boundary}\r\nContent-Type: image/{type}\r\nContent-Length: {length}\r\n\r\n"
 )
+SUFFIX = b"\r\n"
 
 CAMERAS = None
 
@@ -43,10 +46,10 @@ CAMERAS = None
 class StreamResponse(flask.Response):
     default_mimetype = "multipart/x-mixed-replace;boundary={boundary}"
 
-    def __init__(self, *args, **kwargs):
-        boundary = kwargs.pop("boundary", BOUNDARY)
+    def __init__(self, *args, boundary=BOUNDARY, **kwargs):
+        self.boundary = boundary
         mimetype = kwargs.pop("mimetype", self.default_mimetype)
-        kwargs["mimetype"] = mimetype.format(boundary=boundary)
+        kwargs["mimetype"] = mimetype.format(boundary=self.boundary)
         super().__init__(*args, **kwargs)
 
 
@@ -80,12 +83,17 @@ class Trigger:
 
 
 class Camera:
-    def __init__(self, device: Device) -> None:
+    def __init__(self, device: Device, frame_type='jpeg') -> None:
         self.device: Device = device
         self.runner: gevent.Greenlet | None = None
         self.last_frame: bytes | None = None
         self.last_request: float = 0.0
         self.trigger: Trigger = Trigger()
+        self.frame_type = frame_type
+
+    def __iter__(self):
+        while True:
+            yield self.next_frame()
 
     def next_frame(self) -> bytes:
         self.last_request = gevent.time.monotonic()
@@ -110,11 +118,16 @@ class Camera:
 
     def run(self):
         with self.device:
+            capture = VideoCapture(self.device)
+            capture.set_format(640, 480, "MJPG")
+            format = capture.get_format()
+            to_frame = buffer_to_frame_maker(format, output=self.frame_type)
             self.last_request = gevent.time.monotonic()
-            for i, frame in enumerate(self.device):
+            buff = io.BytesIO()
+            for i, data in enumerate(self.device):
+                self.last_frame = to_frame(data)
                 if not i % 100:
-                    self.device.log.info(f"frame {i:04d}")
-                self.last_frame = frame
+                    self.device.log.info(f"frame {i:04d} (raw={len(data) / 1000:.1f} kb; {self.frame_type}={len(self.last_frame) / 1000:.1f} kb)")
                 self.trigger.set()
                 if gevent.time.monotonic() - self.last_request > 10:
                     self.device.log.info("Stopping camera task due to inactivity")
@@ -134,9 +147,31 @@ def cameras() -> list[Camera]:
     return CAMERAS
 
 
-def jpeg_frame(frame, boundary=BOUNDARY):
-    header = HEADER.format(type="jpeg", boundary=boundary, length=len(frame)).encode()
-    return b"".join((header, frame, SUFFIX))
+def buffer_to_frame_maker(format: Format, output='avif'):
+    match (format.pixel_format, output.lower()):
+        case [PixelFormat.JPEG | PixelFormat.MJPEG, 'jpeg']:
+            return functools.partial(to_frame, type='jpeg')
+        case _:            
+            return functools.partial(buffer_to_frame, format=format, output=output)
+
+
+def buffer_to_frame(data: bytes, format: Format, output='jpeg'):
+    from PIL import Image
+    match format.pixel_format:
+        case PixelFormat.JPEG | PixelFormat.MJPEG:
+            image = Image.open(io.BytesIO(data))
+        case PixelFormat.GREY:
+            image = Image.frombuffer("L", (format.width, format.height), data)
+        case _:
+            raise ValueError(f"unsupported pixel format {format.pixel_format}")
+    buff = io.BytesIO()
+    image.save(buff, output)
+    return to_frame(buff.getvalue(), type=output)
+
+
+def to_frame(data, type='avif', boundary=BOUNDARY):
+    header = HEADER.format(type=type, boundary=boundary, length=len(data)).encode()
+    return b"".join((header, data, SUFFIX))
 
 
 @app.get("/")
@@ -164,19 +199,32 @@ def stop(device_id):
 @app.get("/camera/<int:device_id>")
 def device(device_id: int):
     camera = cameras()[device_id]
-    return flask.render_template("device.html", camera=camera)
+    with camera.device:
+        return flask.render_template("device.html", camera=camera)
 
 
 @app.get("/camera/<int:device_id>/stream")
 def stream(device_id):
     camera = cameras()[device_id]
+    return StreamResponse(iter(camera))
 
-    def send_frames():
-        while True:
-            yield jpeg_frame(camera.next_frame(), boundary=BOUNDARY)
 
-    return StreamResponse(send_frames(), boundary=BOUNDARY)
+@app.post("/camera/<int:device_id>/control/<int:control_id>")
+def set_control(device_id, control_id):
+    camera = cameras()[device_id]
+    with camera.device:
+        control = camera.device.controls[control_id]
+        control.value = int(flask.request.form["value"])
+    return "", 204
 
+
+@app.post("/camera/<int:device_id>/control/<int:control_id>/reset")
+def reset_control(device_id, control_id):
+    camera = cameras()[device_id]
+    with camera.device:
+        control = camera.device.controls[control_id]
+        control.value = control.info.default_value
+    return flask.render_template("control.html", control=control)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
