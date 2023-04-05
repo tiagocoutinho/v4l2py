93270fe65a95eb5d51a0b623565d93c0219f6c56
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-04-05 20:15:17 +0200

    Switch from pytest to ward; Better CI

--- setup.py
+++ setup.py
@@ -6,43 +6,6 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-with open("README.md") as f:
-    readme = f.read()
-
-
-setup(
-    name="v4l2py",
-    version="2.1.0",
-    author="Jose Tiago Macara Coutinho",
-    author_email="coutinhotiago@gmail.com",
-    license="GPLv3",
-    keywords="v4l2, video4linux, video4linux2, human",
-    description="Human friendly video for linux",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Multimedia :: Video",
-        "Topic :: Multimedia :: Video :: Capture",
-    ],
-    url="http://pypi.python.org/pypi/v4l2py",
-    project_urls={
-        "Documentation": "https://github.com/tiagocoutinho/v4l2py",
-        "Source": "https://github.com/tiagocoutinho/v4l2py",
-    },
-)
+setup()
