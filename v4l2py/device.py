f4b5fd8ef672ebeca4709c5693f934d9d61a7142
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2024-02-16 09:01:42 +0100

    Add missing import

--- v4l2py/device.py
+++ v4l2py/device.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2021 Tiago Coutinho
 # Distributed under the GPLv3 license. See LICENSE for more info.
 
-# ruff: noqa: F403
+# ruff: noqa: F401, F403
 
+from linuxpy.device import device_number
 from linuxpy.video.device import *
